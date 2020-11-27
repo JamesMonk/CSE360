6e21a6011dd3a493fa7c4622efb46c9367d51179
chopi <chopinguy@cox.net>
2020-11-27 12:26:56 -0700

    James Monk 11/26/2020 12:51 PM

--- src/cse360project/Main.java
+++ src/cse360project/Main.java
@@ -10,11 +10,11 @@ public class Main extends JFrame implements ActionListener{
     private JPanel p;
     private JButton test;
     private JMenuBar menuBar;
-    private JMenu file, about;
-    private JMenuItem load, addAttendance, save, plot;
+    private JMenu file;
+    private JMenuItem load, addAttendance, save, plot, about;
     public Main () {
-        JFrame.setDefaultLookAndFeelDecorated(true);
-        JDialog.setDefaultLookAndFeelDecorated(true);
+//        JFrame.setDefaultLookAndFeelDecorated(true);
+//        JDialog.setDefaultLookAndFeelDecorated(true);
         JFrame frame = new JFrame("CSE360 Final Project");
         frame.setLayout(new FlowLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -24,11 +24,12 @@ public class Main extends JFrame implements ActionListener{
         file = new JMenu("file");
         menuBar.add(file);
 
-        about = new JMenu("about");
+        about = new JMenuItem("about");
+        about.addActionListener(new ListenToAbout());
         menuBar.add(about);
 
         load = new JMenuItem("Load a Roster");
-        load.addActionListener(new ListenToLoad());
+        load.addActionListener(new ListenToLoad(frame));
         addAttendance = new JMenuItem("Add Attendance");
         addAttendance.addActionListener(new ListenToAdd());
         save = new JMenuItem("Save");
@@ -62,10 +63,22 @@ public class Main extends JFrame implements ActionListener{
     }
 
     public class ListenToLoad implements ActionListener {
+        private JFrame frame;
 
+        public ListenToLoad(JFrame frame) {
+            this.frame = frame;
+        }
         @Override
         public void actionPerformed(ActionEvent e) {
-            System.out.println("Load");
+            Object[] column = {"One", "Two"};
+            Object[][]data = {{1, 2}, {3, 4}, {5, 6}};
+
+            JTable table = new JTable(data, column);
+            JScrollPane pane = new JScrollPane(table);
+            JPanel panel = new JPanel();
+            panel.add(pane);
+            this.frame.add(new JScrollPane(panel));
+            this.frame.setVisible(true);
         }
     }
 
@@ -87,6 +100,18 @@ public class Main extends JFrame implements ActionListener{
 
         public void actionPerformed(ActionEvent e) {
             System.out.println("Plot");
+        }
+    }
+
+    public class ListenToAbout extends JFrame implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+
+//            System.out.println("About");
+            JFrame frame = new JFrame("About");
+            frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            JOptionPane.showMessageDialog(frame, "CSE360 Final Project\nNames:\nJames Monk\nCullen Walsh");
+
         }
     }
 }
